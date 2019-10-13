97b34c1da16875db05d30551ceb35deae30a8e49
ridait <rejra2012@gmail.com>
2019-10-13 21:15:47 +0100

    add configuration (restrict some endpoints and permit others)

--- spring-security/src/main/java/com/ridait/springsecurity/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/ridait/springsecurity/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/secret").authenticated();
+        http.authorizeRequests().anyRequest().permitAll();
     }
 }
