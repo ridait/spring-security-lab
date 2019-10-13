102e1374c5b74f3c57d3858647eb78a164ca0dce
ridait <rejra2012@gmail.com>
2019-10-13 22:10:18 +0100

    add new MVC Controller and use form login instead of basic http

--- spring-security/src/main/java/com/ridait/springsecurity/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/ridait/springsecurity/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        http.formLogin();
         http.authorizeRequests().antMatchers("/secret").authenticated();
         http.authorizeRequests().anyRequest().permitAll();
     }
